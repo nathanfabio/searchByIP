70f77f1e75003a6be8ba579fcbe80fd294fd0840
Nathan Fabio <nathan_3_m@hotmail.com>
2023-01-29 20:01:19 -0300

    application working, but not doing anything yet

--- searchByIP.go
+++ searchByIP.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"github.com/nathanfabio/searchByIP.git/app"
+	"os"
+	"log"
+)
+
 func main() {
-	
+	application := app.Generate()
+	if err := application.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
 }
